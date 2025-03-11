85ec96f1a4faf3626e9ad546854a92a8d9d56e64
Mandie02 <mandie121605@gmail.com>
2025-03-12 00:04:30 +0800

    img upload

--- database/db.php
+++ database/db.php
@@ -18,10 +18,11 @@
     $grade_section = isset($data['grade_section']) ? $data['grade_section'] : null;
     $adviser = isset($data['adviser']) ? $data['adviser'] : null;
     $NFC_CARD_KEY = isset($data['NFC_CARD_KEY']) ? $data['NFC_CARD_KEY'] : null;
-
-    if ($email && $last_name && $first_name && $lrn && $grade_section && $adviser && $NFC_CARD_KEY) {
-        $stmt = $conn->prepare("INSERT INTO nfcdata (email, lastname, firstname, lrn, gradeSection, adviser, nfcCardKey) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssssss", $email, $last_name, $first_name, $lrn, $grade_section, $adviser, $NFC_CARD_KEY);
+    $img_profile = isset($data['img_profile']) ? $data['img_profile'] : null;
+    
+    if ($email && $last_name && $first_name && $lrn && $grade_section && $adviser && $NFC_CARD_KEY && $img_profile) {
+        $stmt = $conn->prepare("INSERT INTO nfcdata (email, lastname, firstname, lrn, gradeSection, adviser, nfcCardKey, user_img) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssssss", $email, $last_name, $first_name, $lrn, $grade_section, $adviser, $NFC_CARD_KEY, $img_profile);
     
         if ($stmt->execute()) {
             echo json_encode(["success" => "Data inserted successfully"]);
@@ -31,31 +32,4 @@
     
         $stmt->close();
     }
-
-
-
-    // Get the nfckey from the database end pass to javascript scan.js file
-    $nfc_key = isset($_GET['nfc_key']) ? $_GET['nfc_key'] : null;
-
-    if ($nfc_key) {
-        $stmt = $conn->prepare("SELECT email, lastname, firstname, lrn, gradeSection, adviser, nfcCardKey FROM nfcdata WHERE nfcCardKey = ?");
-        $stmt->bind_param("s", $nfc_key);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        $data = [];
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $data[] = $row;
-            }
-            echo json_encode($data);
-        } else {
-            echo json_encode(["error" => "No data found for the provided NFC key"]);
-        }
-        $stmt->close();
-    } else {
-            echo json_encode(["error" => "NFC key is required"]);
-    }
-
-    $conn->close();
 ?>
