ad1622bda331926066382aefa715d9a96466eccf
Mandie02 <mandie121605@gmail.com>
2025-03-12 00:00:04 +0800

    added config for fetching data to db

---
+++ pages/Register/register.config.php
@@ -0,0 +1,26 @@
+<?
+
+
+    $data = json_decode(file_get_contents("php://input"), true);
+    $email = isset($data['email']) ? $data['email'] : null;
+    $last_name = isset($data['last_name']) ? $data['last_name'] : null;
+    $first_name = isset($data['first_name']) ? $data['first_name'] : null;
+    $lrn = isset($data['lrn']) ? $data['lrn'] : null;
+    $grade_section = isset($data['grade_section']) ? $data['grade_section'] : null;
+    $adviser = isset($data['adviser']) ? $data['adviser'] : null;
+    $NFC_CARD_KEY = isset($data['NFC_CARD_KEY']) ? $data['NFC_CARD_KEY'] : null;
+    $img_profile = isset($data['img_id']) ? $data['img_id'] : null;
+
+    if ($email && $last_name && $first_name && $lrn && $grade_section && $adviser && $NFC_CARD_KEY && $img_profile) {
+        $stmt = $conn->prepare("INSERT INTO nfcdata (email, lastname, firstname, lrn, gradeSection, adviser, nfcCardKey, user_img) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssssss", $email, $last_name, $first_name, $lrn, $grade_section, $adviser, $NFC_CARD_KEY, $img_profile);
+    
+        if ($stmt->execute()) {
+            echo json_encode(["success" => "Data inserted successfully"]);
+        } else {
+            echo json_encode(["error" => "Error inserting data: " . $stmt->error]);
+        }
+    
+        $stmt->close();
+    }
+?>
